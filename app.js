bb4a1066263bb205989275b7976ed5ad792a3212
Jeremy Ball <jball@invaluable.com>
2014-07-10 08:58:15 -0400

    Changing port to 4001.

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4001);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
